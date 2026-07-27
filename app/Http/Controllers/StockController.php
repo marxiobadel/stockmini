7a11ef7a43c80550398100bc59917f71ee42f1e0
Marxio Badel <marxiobadel@gmail.com>
2026-07-27 01:53:08 +0100

    code review

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -11,6 +11,7 @@ use App\Models\Supplier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
+use Illuminate\Validation\ValidationException;
 
 class StockController extends Controller
 {
